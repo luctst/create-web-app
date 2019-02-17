5eb049a7bb8748557184980e81b71e178cb12745
Lucas Tostée <lucas.tostee@gmail.com>
2019-02-18 00:03:12 +0100

    New entry point index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
  * Import
  */
 import "./assets/scss/main.scss";
-import img from "./assets/img/react-icon.png";
 import React from "react";
 import { render } from "react-dom";
+import App from "./view/app";
 
 /**
  * Variables
@@ -14,19 +14,11 @@ const app = document.querySelector("#root");
 /**
  * Déclaration
  */
-export class App extends React.Component {
+class Main extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <header>
-                    <img src={img} className="is__img"/>
-                    <h1 className="is__main__title">Web App Boilerplate</h1>
-                </header>
-                <section className="container content">
-                    <h2 className="is__title__secondary">Welcome on your App !! 👋 but what to do now ? </h2>
-                    <p>You can start by editing the index.js file to change your app 👨‍💻 .</p>
-                    <p>If you want more information's of how to use this app check this link 👉 <a href="https://github.com/luctst/web-app-boilerplate">README.md</a> 👈 .</p>
-                </section>
+                <App/>
             </React.Fragment>
         );
     }
@@ -35,4 +27,4 @@ export class App extends React.Component {
 /**
  * Éxécution
  */
-render(<App/>, app);
+render(<Main/>, app);
