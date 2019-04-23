43cf1dff981b7f36e0a06f01a6ec6f49cbcecd6a
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-23 02:40:10 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,15 @@
-/**
- * Variables
- */
-const path = require("path");
-const extractCss = require("mini-css-extract-plugin");
-const html = require("html-webpack-plugin");
+const config = require("./webpack/config");
+const plugins = require("./webpack/plugins-config");
+const modules = require("./webpack/module-config");
 
 /**
  * Export
  */
-module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, "src", "index.js"),
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, "public"),
-        compress: true,
-        port: 8080,
-        open: true,
-        disableHostCheck: true,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [extractCss.loader, "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(jpg|png|svg|jpeg|gif)$/,
-                use: [{
-                    loader: "file-loader",
-                    options: {name:"[name].[ext]",outputPath: "img/"}
-                }]
-            }
-        ]
-    },
-    plugins: [
-        new extractCss({
-            filename: "style.css"
-        })
-    ]
+module.exports = () => {
+	const getConfig = {...config};
+	return {
+		...getConfig,
+		module: modules,
+		plugins,
+	};
 };
