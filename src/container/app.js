143471cdb5d63cceae9776f33aa09d994dc91093
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-28 20:18:13 +0200

    Update

--- src/container/app.js
+++ src/container/app.js
@@ -3,7 +3,6 @@ import Header from "../components/Header";
 import Content from "../components/Content";
 import ErrorBoundary from "./ErrorBoundary";
 
-
 /**
  * The app component hold all the views available for our app.
  */
