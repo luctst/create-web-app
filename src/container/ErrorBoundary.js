8b3e5a4ba2d2aae497c0092048a1a2e94db5a1f2
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-28 20:17:53 +0200

    Create ErrorBoundary component

--- src/container/ErrorBoundary.js
+++ src/container/ErrorBoundary.js
@@ -1,28 +1,77 @@
 import React from "react";
 import styled from "styled-components";
+import Container from "../styles/layout/Container";
 
+/**
+ * Styled-components
+ */
+const Header = styled(Container)`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	span:hover {
+		cursor: pointer;
+	}
+`;
+const Main = styled(Container)`
+	details {
+		margin-top: 5%;
+	}
+
+	details > div {
+		border-radius: 5px;
+		padding: 10px 20px;
+		background-color: #EEE;
+	}
+`;
+
+/**
+ * Render the error with good UI interface.
+ */
 class ErrorBoundary extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { hasError: false };
-	}
-
-	static getDerivedStateFromError(error) {
-		// Update state so the next render will show the fallback UI.
-		return { hasError: true };
+		this.state = {
+			hasError: false,
+			error: null
+		};
+		this.handleClick = this.handleClick.bind(this);
 	}
 
 	componentDidCatch(error, info) {
-		// You can also log the error to an error reporting service
-		logErrorToMyService(error, info);
+		this.setState({
+			hasError: true,
+			error: error
+		})
+	}
+
+	handleClick() {
+		this.setState({hasError: false});
 	}
 
 	render() {
 		if (this.state.hasError) {
-			// You can render any custom fallback UI
-			return <h1>Something went wrong.</h1>;
+			const firstParsing = this.state.error.stack.split("(webpack:///./");
+			const secondParsing = firstParsing[1].split("?");
+			return (
+				<>
+					<Header as="header">
+						<h1>Error: {this.state.error.message}</h1>
+						<span onClick={this.handleClick}>X</span>
+					</Header>
+					<Main as="main">
+						<p>The error happen in the <code>{secondParsing[0]}</code> file.</p>
+						<p>At the line <code>{secondParsing[1].split(":")[1]}</code>.</p>
+						<details>
+							<div>
+								<code>{this.state.error.stack}</code>
+							</div>
+						</details>
+					</Main>
+				</>
+			);
 		}
-
 		return this.props.children;
 	}
 }
